bf1086e8dc3f2812f7656758ad5e2e10f560e16d
Talha Haider <talhahaider074@gmail.com>
2025-01-29 09:43:09 +0500

    wallet issue fixed

--- services/wallet.service.js
+++ services/wallet.service.js
@@ -4,7 +4,7 @@ const { Wallet } = require("../models/wallet.history");
 const getWalletHistory = async (req,res)=>{
     try {
         let data = await Wallet.find({accountId:req.params.id})
-        return res.status(200).json({ data:data,msg:bull,status:200 });
+        return res.status(200).json({ data:data,msg:null,status:200 });
     } 
     catch (error) {
         console.log(error)
