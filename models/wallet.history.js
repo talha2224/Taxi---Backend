1fb0b76fd5b88a17c1ebc102f81338944c13fe3b
Talha Haider <talhahaider074@gmail.com>
2025-01-29 10:16:06 +0500

    booking id added

--- models/wallet.history.js
+++ models/wallet.history.js
@@ -6,7 +6,8 @@ const walletSchema = mongoose.Schema({
     accountId:{type:mongoose.Schema.Types.ObjectId,ref:"Account",required:true},
     type:{type:String,default:false},
     amount:{type:Number,default:false},
-    msg:{type:String,default:false}
+    msg:{type:String,default:false},
+    bookingId:{type:mongoose.Schema.Types.ObjectId,ref:"Booking",required:true}
 })
 
 
